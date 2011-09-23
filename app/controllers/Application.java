6aa62d69ce20c72d3d682c40c992afc2474d85f8
Guilherme Elias <guilherme.elias@gmail.com>
2011-09-23 01:46:00 -0300

    Created first app using play framework

---
+++ app/controllers/Application.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    public static void feedback() {
+	String message = "Yes, play is very cool";
+	long count = Feedback.count();
+	render(count);
+	render(message);
+    }
+
+}
---
+++ app/models/Feedback.java
@@ -0,0 +1,22 @@
+package models;
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Feedback extends Model {
+
+	public String name;
+	public boolean like;
+	public boolean dislike;
+
+	public Feedback(boolean feedback) {
+		if (feedback) {
+			this.like = feedback;
+			this.dislike = !feedback;
+		} else {
+			this.like = feedback;
+			this.dislike = !feedback;
+		}
+	}
+}
